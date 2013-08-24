0b649899cd1a97a8f56b1a68223d3f2803762080
Annabelle Rolland <annabellerolland@gmail.com>
2013-08-24 13:04:41 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flatbuddy::Application.config.secret_key_base = '39e2f21b63c5ed5cb856f6bd87b2993f6f3d260a9db65edf37f977f9557e02a84c3c0cc616a45d299abb41963b6f62aa5ea9a6c4af34be73fa970eb0bf096470'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flatbuddy::Application.config.session_store :cookie_store, key: '_flatbuddy_session'
